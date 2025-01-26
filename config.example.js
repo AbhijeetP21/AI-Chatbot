95214cedb15432608ad1190fd917336ad1c5d5e1
Abhijeet S Pachpute <abhijeetsp@Abhijeets-Mac-mini.local>
2025-01-25 22:36:49 -0700

    config example and workflow for gitpages

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Rename this file to config.js and add your API key
+export default {
+  apiEndpoint: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent",
+  apiKey: "YOUR_API_KEY" // Replace with your actual API key
+};
